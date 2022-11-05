043139e2e42d182cddacf85e61a4e6ffa7bf2e06
David <david.felder3@gmail.com>
2022-11-05 15:36:38 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function toShop() {
+    location.href = './shop';
+}
+
+function toTwitch() {
+    window.open('https://twitch.tv/screetox', '_blank').focus();
+}
+
+function toYouTube() {
+    window.open('https://www.youtube.com/channel/UCErEad8EovKrByoIZPC6yhQ?sub_confirmation=1', '_blank').focus();
+}
---
+++ js/shop.js
@@ -0,0 +1,8 @@
+var spread_shop_config = {
+    shopName: 'screetox',
+    prefix: 'https://screetox.myspreadshop.de',
+    baseId: 'myspreadshop',
+    locale: 'de_DE',
+    updateMetadata: false,
+    usePushState: true,
+};
